969633d1a9a2c4bd222178a3760258ad36d311be
Kamakepar2029 <58629552+Kamakepar2029@users.noreply.github.com>
2020-10-03 15:00:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,3 +17,12 @@ bot = telebot.TeleBot(config.TOKEN)
 @bot.message_handler(commands=['start'])
 def welcome(message):
   bot.send_message(message.chat.id,'This bot can give you html code of the requested site :-)')
+
+@bot.message_handler(content_types=['text'])
+def lalala(message):
+  if (message.text == 'Hello'):
+    bot.send_message(message.chat.id, 'Hello Man')
+  else:
+    u = 'Your message was: '+str(message.text)+' :-)'
+    bot.send_message(message.chat.id, u)
+bot.polling(none_stop=True)
